78c823fcdcaa67c6536f95129e24e635d2d27e29
Anju Chamantha <chamantha97anju@gmail.com>
2025-03-03 07:37:29 +0530

    [ConsentMgr] Bug fix in checking users who can revoke consent

--- components/org.wso2.openbanking.cds.consent.extensions/src/main/java/org/wso2/openbanking/cds/consent/extensions/admin/impl/CDSConsentAdminHandler.java
+++ components/org.wso2.openbanking.cds.consent.extensions/src/main/java/org/wso2/openbanking/cds/consent/extensions/admin/impl/CDSConsentAdminHandler.java
@@ -251,10 +251,9 @@ public class CDSConsentAdminHandler implements ConsentAdminHandler {
      */
     private boolean isPrimaryUserRevoking(DetailedConsentResource detailedConsentResource, String userID) {
         for (AuthorizationResource authorizationResource : detailedConsentResource.getAuthorizationResources()) {
-            if (userID.equals(authorizationResource.getUserID()) &&
-                    (AUTH_RESOURCE_TYPE_PRIMARY.equals(authorizationResource.getAuthorizationType()) ||
-                            NOMINATED_REPRESENTATIVE.equals(authorizationResource.getAuthorizationType()))) {
-                return true;
+            if (userID.equals(authorizationResource.getUserID())) {
+                return AUTH_RESOURCE_TYPE_PRIMARY.equals(authorizationResource.getAuthorizationType()) ||
+                        NOMINATED_REPRESENTATIVE.equals(authorizationResource.getAuthorizationType());
             }
         }
         return true;
