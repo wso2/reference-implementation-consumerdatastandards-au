50fc29d1d51846c53832c9acb6684c8402867f1d
Imesh Perera <imeshu@wso2.com>
2025-11-10 15:51:54 +0530

    Fix review comments

--- components/org.wso2.openbanking.cds.common/src/main/java/org/wso2/openbanking/cds/common/error/handling/util/ErrorConstants.java
+++ components/org.wso2.openbanking.cds.common/src/main/java/org/wso2/openbanking/cds/common/error/handling/util/ErrorConstants.java
@@ -181,8 +181,8 @@ public class ErrorConstants {
         INVALID_ACCEPT_HEADER(406, "urn:au-cds:error:cds-all:GeneralError/Expected", "Expected Error Encountered",
                 "Invalid Accept Header"),
         //Page out of range
-        PAGE_OUT_OF_RANGE(422, "urn:au-cds:error:cds-all: Field/InvalidPageSize", "Invalid Page Size",
-                "Page Requested Is Out Of Range"),
+        PAGE_OUT_OF_RANGE(422, "urn:au-cds:error:cds-all:Field/InvalidPageSize", "Invalid Page Size",
+                "Page Size Value Exceeds Valid Limits"),
         //Page size is greater than max
         PAGE_SIZE_EXCEED(422, "AU.CDR.Invalid.PageSizeTooLarge", "Page Size Exceeded", "Page Size Exceeded"),
         //Requested version is not a positive integer
